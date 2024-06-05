e12cd2a90af70160d309099a6db6a1a8c04aa5ba
victor-ok <vua.okafor@gmail.com>
2024-06-05 18:02:41 +0100

    fixed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# incorrect file name
+exec{ 'fix-wordpress':
+    command => 'sed -i \'s/class-wp-locale.phpp/class-wp-locale.php/g\' /var/www/html/wp-settings.php',
+    path    => '/usr/local/bin/:/bin/',
 }
