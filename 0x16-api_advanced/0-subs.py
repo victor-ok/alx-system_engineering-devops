ce4b76afd346c7beca9c1ce094008958ff8912de
victor-ok <vua.okafor@gmail.com>
2024-08-14 10:02:27 +0100

    validation fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,38 @@
+# #!/usr/bin/python3
+# """Function to query subscribers on a given Reddit subreddit."""
+# import requests
+
+
+# def number_of_subscribers(subreddit):
+#     """Return the total number of subscribers on a given subreddit."""
+#     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+#     headers = {
+#         "User-Agent": "my bot 0.1"
+#     }
+#     response = requests.get(url, headers=headers, allow_redirects=False)
+#     if response.status_code == 404:
+#         return 0
+#     results = response.json().get("data")
+#     return results.get("subscribers")
+
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""
+module for number of subscribers for subreddit
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """
+    number of subscribers for subreddit
+    """
+    results = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/about.json",
+        headers={'User-agent': 'my bot 0.1'})
+
+    if results.status_code == 200:
+        response_data = results.json()
+        subscribers = response_data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
